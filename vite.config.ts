8c57e84a5ce5ae788e422c797f7dbb36d66a4eb9
utaware <1264051408@qq.com>
2026-08-27 18:00:07 +0800

    feat: add router、store、ui-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
