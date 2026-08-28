9aadcfc42161815c98418514624e1d3f2cd6d58f
utaware <1264051408@qq.com>
2026-08-28 09:47:10 +0800

    feat: 调整项目路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/HomeView.vue'),
+      component: () => import('@/views/Home/index.vue'),
+    },
+    {
+      path: '/jigsaw',
+      name: 'jigsaw',
+      component: () => import('@/views/Jigsaw/index.vue'),
     },
   ],
 })
